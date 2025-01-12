f1719eb94433645117e7b22a4a0de5b16686774b
forgotten-xyz <93533394+forgotten-xyz@users.noreply.github.com>
2025-01-12 21:09:17 +0300

    fix handlers

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 import sys
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def notify_agents(context: CallbackContext):
